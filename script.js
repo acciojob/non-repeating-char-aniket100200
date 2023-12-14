c3185c179c303f821f4a7f919f5892bed584e8f0
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-14 13:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(str) 
+{
+	// Write your code here
+	
+
+	let obj={};
+	for (const key in str) 
+	{
+		obj[str.charAt(key)]=0;
+	}
+
+	for (const val of str) {
+		obj[val]++;
+	}
+	// console.log(obj);
+
+	for (const key in obj) 
+	{
+		if(obj[key]===1)
+		{
+
+			return key;
+		}
+	}
+
+	
+
+	return null;
+
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+   const input = prompt("Enter a string");
+   alert(firstNonRepeatedChar(input));
+
+// firstNonRepeatedChar("aabbcdd");   
+   
+   
